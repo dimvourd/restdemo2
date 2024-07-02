e539b53ef9a1f4df0a8351fcbb2eb1526da3f2e1
VourdoukasD <dimvourd@gmail.com>
2024-07-02 12:04:13 +0300

    Student POJO with Rest Controller
    @PathVariable

--- src/main/java/com/luv2code/demo/rest/StudentController.java
+++ src/main/java/com/luv2code/demo/rest/StudentController.java
@@ -2,6 +2,7 @@ package com.luv2code.demo.rest;
 
 
 import com.luv2code.demo.pojo.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +15,29 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentController {
+    private List<Student> theStudents;
+
+    @PostConstruct
+    private void initStudents(){
+        theStudents = new ArrayList<>(3);
+        theStudents.add(new Student("dim", "vrd"));
+        theStudents.add(new Student("john", "vrd"));
+        theStudents.add(new Student("sophia", "vrd"));
+    }
+
 
     @GetMapping("/students")
     public List<Student> getAllStudents(){
-        List<Student> sts = new ArrayList<>(3);
-        sts.add(new Student("dim", "vrd"));
-        sts.add(new Student("john", "vrd"));
-        sts.add(new Student("sophia", "vrd"));
 
-        return sts;
+        return theStudents;
     }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudentById(@PathVariable("studentId") Integer Id){
+
+        return theStudents.get(Id);
+    }
+
 
 
 }
